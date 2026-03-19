5f2c8e71fbca653b7eeffa3128632812a0dca748
nutricaocommarco <manjunior007@gmail.com>
2026-03-19 12:15:17 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// 1. Importar o componente da Vercel
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    {/* 2. Adicionar o componente aqui embaixo do App */}
+    <SpeedInsights />
   </React.StrictMode>,
 )
