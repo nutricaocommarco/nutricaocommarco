9184d5f74848cb3572bdb64590a09ccc53676f96
nutricaocommarco <manjunior007@gmail.com>
2026-03-19 12:31:50 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-// 1. Importar o componente da Vercel
+
+// 1. Ferramentas de monitoramento da Vercel
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    {/* 2. Adicionar o componente aqui embaixo do App */}
+    {/* 2. Ligando as duas ferramentas abaixo do App */}
     <SpeedInsights />
+    <Analytics />
   </React.StrictMode>,
 )
