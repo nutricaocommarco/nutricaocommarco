e017433ee9e3fc8834f81d1bd2c600792c71787a
nutricaocommarco <manjunior007@gmail.com>
2026-03-21 14:13:00 -0300

    Integrate vite-plugin-prerender with routes configuration
    Added vite-plugin-prerender to configure static file generation and specified routes for prerendering.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import vitePrerender from 'vite-plugin-prerender'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vitePrerender({
+      // Onde os arquivos finais vão ficar
+      staticDir: path.join(__dirname, 'dist'),
+      // As rotas que você quer que o WhatsApp reconheça a imagem
+      routes: [
+        '/', 
+        '/blog', 
+        '/efeito_sanfona_inflamacao_invisivel', 
+        '/hormonios_da_fome_emagrecimento'
+      ],
+    }),
+  ],
 })
