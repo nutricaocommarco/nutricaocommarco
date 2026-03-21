b30a985a937b2e31a93f3151b37630005b671d38
nutricaocommarco <manjunior007@gmail.com>
2026-03-21 18:18:10 -0300

    Update Newsletter import path in Bioimpedancia.jsx

--- src/pages/Bioimpedancia.jsx
+++ src/pages/Bioimpedancia.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { ChevronLeft, PlayCircle, HelpCircle, Zap, Headphones } from 'lucide-react';
 import ArtigosRecomendados from '../components/ArtigosRecomendados';
-import Newsletter from './Newsletter';
+import Newsletter from '../components/Newsletter';
 import { Helmet } from 'react-helmet-async';
 
 const githubImgBase = "https://raw.githubusercontent.com/nutricaocommarco/nutricaocommarco/main/Imagens/";
