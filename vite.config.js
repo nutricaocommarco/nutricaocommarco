f54a93c9f4b8227d89d741f92719520bb939836d
nutricaocommarco <manjunior007@gmail.com>
2026-03-21 14:27:35 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Arquivo limpo para não dar erro de módulo não encontrado
 export default defineConfig({
   plugins: [react()],
 })
