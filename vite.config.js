8b91d7d9deaf50985048c3d3f9b1e547af4cc898
nutricaocommarco <manjunior007@gmail.com>
2026-03-21 14:13:45 -0300

    Update Vite config with new pre-rendering routes
    Added additional routes for pre-rendering in Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,21 @@ export default defineConfig({
   plugins: [
     react(),
     vitePrerender({
-      // Onde os arquivos finais vão ficar
+      // Define a pasta de saída (para o Vite é sempre 'dist')
       staticDir: path.join(__dirname, 'dist'),
-      // As rotas que você quer que o WhatsApp reconheça a imagem
+      
+      // Lista de páginas para pré-renderizar
       routes: [
         '/', 
         '/blog', 
         '/efeito_sanfona_inflamacao_invisivel', 
-        '/hormonios_da_fome_emagrecimento'
+        '/hormonios_da_fome_emagrecimento',
+        '/quantas_frutas_posso_comer',
+        '/vitamina_a_para_que_serve',
+        '/o_que_e_antropometria',
+        '/a_balanca_de_bioimpedancia_e_confiavel',
+        '/o_dilema_do_sangue_na_altitude',
+        '/por_que_o_feijao_da_gases'
       ],
     }),
   ],
