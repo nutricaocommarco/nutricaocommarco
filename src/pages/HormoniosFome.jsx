ee71d00703c3dc2f516b14a61e4fa2598a206c79
nutricaocommarco <manjunior007@gmail.com>
2026-03-21 21:02:52 -0300

    Update HormoniosFome.jsx

--- src/pages/HormoniosFome.jsx
+++ src/pages/HormoniosFome.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { ChevronLeft, HelpCircle, BrainCircuit, Scale, Pill, Flame, Target, Headphones, PlayCircle } from 'lucide-react';
 import ArtigosRecomendados from '../components/ArtigosRecomendados';
+import Newsletter from '../components/Newsletter';
 import { Helmet } from 'react-helmet-async';
 
 const githubImgBase = "https://raw.githubusercontent.com/nutricaocommarco/nutricaocommarco/main/Imagens/";
@@ -157,7 +158,7 @@ export default function HormoniosFome() {
                   </div>
                 </div>
               </div>
-
+<Newsletter />
             </div>
           </article>
 
