d14921930a0a93abcb24fa37cdcec8da135b6896
nutricaocommarco <manjunior007@gmail.com>
2026-03-24 15:54:57 -0300

    Atualizar o retratrutida.jsx

--- src/pages/retratrutida.jsx
+++ src/pages/retratrutida.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation, Link } from 'react-router-dom';
-import { Helmet } from 'react-helmet-async';
+import { Helmet } from 'react-helmet';
 import { ChevronLeft, Headphones, Activity, ChevronRight, Leaf, Scale, Heart, HelpCircle, PlayCircle } from 'lucide-react';
 
 // Importando componentes fixos
