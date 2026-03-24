8253ee578d29ce13cd0e7634fa18c6228b3e3f1b
nutricaocommarco <manjunior007@gmail.com>
2026-03-23 21:23:15 -0300

    Atualizar o Eritropoetina.jsx

--- src/pages/Eritropoetina.jsx
+++ src/pages/Eritropoetina.jsx
@@ -34,6 +34,7 @@ export default function Eritropoetina() {
             "author": {"@type": "Person", "name": "Marco Aurélio Jr.", "url": "https://www.nutricaocommarco.com.br/sobre"},
             "publisher": {"@type": "Organization", "name": "Nutrição com Marco", "logo": {"@type": "ImageObject", "url": "https://raw.githubusercontent.com/nutricaocommarco/nutricaocommarco/main/Imagens/logoN_pingus.png"}},
             "datePublished": "2026-03-20",
+ "dateModified": "2026-03-21",
             "description": "Entenda os riscos fatais do uso sintético do hormônio eritropoetina e da transfusão de hemácias no esporte de alta performance."
           })}
         </script>
