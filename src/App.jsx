412c09268ba6f427b39c40f56755712aff5d347d
nutricaocommarco <manjunior007@gmail.com>
2026-03-26 18:40:00 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -90,7 +90,7 @@ function Layout({ children }) {
         title: 'Tirzepatida: Para que serve? Emagrecimento, Mounjaro e Rebote | Nutrição com Marco',
         desc: 'Descubra para que serve a Tirzepatida (Mounjaro), como funciona no emagrecimento, quanto peso se perde e como evitar o temido efeito rebote.'
       },
-      '/o_que_e_fome_emocional': {
+      '/o-que-e-fome-emocional': {
         title: 'O que é Fome Emocional? Diferenças e Como Controlar | Nutrição com Marco',
         desc: 'Entenda o que é fome emocional, como diferenciá-la da fome física e estratégias práticas para lidar com o impulso de comer por emoção.'
       }
@@ -203,7 +203,7 @@ export default function App() {
             <Route path="/retatrutida_o_que_e" element={<RetatrutidaOQueE />} />
             <Route path="/comer-ovo-todo-dia-aumenta-o-colesterol" element={<OvoColesterol />} />
             <Route path="/tirzepatida-para-que-serve" element={<TirzepatidaParaQueServe />} />
-            <Route path="/o_que_e_fome_emocional" element={<FomeEmocional />} />
+            <Route path="/o-que-e-fome-emocional" element={<FomeEmocional />} />
           </Routes>
         </Layout>
       </Router>
