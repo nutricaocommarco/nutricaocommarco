a84b5dd0ac10a1a56fb7d82b235476826766e15e
nutricaocommarco <manjunior007@gmail.com>
2026-04-14 13:58:51 -0300

    Update prerender.mjs

--- prerender.mjs
+++ prerender.mjs
@@ -41,6 +41,12 @@ const rotasEstaticas = [
     title: 'Avaliação Antropométrica Grátis - Parceria Inatividade Zero | Nutrição com Marco',
     image: `${githubImgBase}PingusReserva.jpg`,
     desc: 'Agende sua avaliação antropométrica gratuita na Academia Inatividade Zero em parceria com Nutrição com Marco.'
+  },
+  { 
+    path: 'confirmacao-av-antropometrica', 
+    title: 'Avaliação Agendada! | Nutrição com Marco', 
+    image: `${githubImgBase}logoN_pingus.png`, 
+    desc: 'Sua avaliação antropométrica foi agendada com sucesso.' 
   }
 ];
 
