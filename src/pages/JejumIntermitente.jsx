9feb0277153bc16a13ccc1f08da6d283edc62aef
nutricaocommarco <manjunior007@gmail.com>
2026-07-14 17:28:28 -0300

    Atualizar o JejumIntermitente.jsx

--- src/pages/JejumIntermitente.jsx
+++ src/pages/JejumIntermitente.jsx
@@ -1,4 +1,4 @@
-Import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import { 
