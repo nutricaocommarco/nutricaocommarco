e99f20fbd1a630208cbe9cb7a15d56cbfbf36347
nutricaocommarco <manjunior007@gmail.com>
2026-07-17 09:19:29 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Esta função varre os arquivos e agrupa tudo o que vem da node_modules
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
